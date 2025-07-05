9fbf60651031f129587a21ead86fde92a67175a8
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-07-06 00:06:33 +0700

    +Add class const value

---
+++ Assets/__Game/Scripts/_Consts/ConstTags.cs
@@ -0,0 +1,9 @@
+public class ConstTags
+{
+    public static readonly string Player = "Player";
+
+
+    //Brick Tags
+    public static readonly string BRICK_PLATFORM = "BrickPlatform";
+    public static readonly string BRICK_BRIDGE = "BrickBridge";
+}

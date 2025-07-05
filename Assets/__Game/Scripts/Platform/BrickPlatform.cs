d5547ffbcb11f2c466c43cbe318d8864c70bcdb1
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-07-05 09:48:29 +0700

    + Add function check meshRenderer

--- Assets/__Game/Scripts/Platform/BrickPlatform.cs
+++ Assets/__Game/Scripts/Platform/BrickPlatform.cs
@@ -2,20 +2,24 @@ using UnityEngine;
 
 public class BrickPlatform : MonoBehaviour, IColorData
 {
-    private MeshRenderer meshRenderer;
+    private MeshRenderer _meshRenderer;
 
+    private void Start()
+    {
+        if(_meshRenderer == null) _meshRenderer = GetComponent<MeshRenderer>();
+    }
 
     public void ChangeMaterialColor(ColorDataType colorDataType, ColorDataConfig colorDataConfig)
     {
-        if (meshRenderer == null)
+        if (_meshRenderer == null)
         {
-            meshRenderer = GetComponent<MeshRenderer>();
-            if (meshRenderer == null)
+            _meshRenderer = GetComponent<MeshRenderer>();
+            if (_meshRenderer == null)
             {
                 Debug.LogError("MeshRenderer component not found on BrickPlatform.");
                 return;
             }
         }
-        meshRenderer.material = colorDataConfig.GetMaterialColor((int)colorDataType);
+        _meshRenderer.material = colorDataConfig.GetMaterialColor((int)colorDataType);
     }
 }

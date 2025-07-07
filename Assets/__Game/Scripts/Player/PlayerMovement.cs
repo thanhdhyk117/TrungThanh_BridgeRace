6198e1c4e94a2b7f6b5e296187bacd4bdafaa7a6
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-07-08 00:51:31 +0700

    + Add script rotate player

--- Assets/__Game/Scripts/Player/PlayerMovement.cs
+++ Assets/__Game/Scripts/Player/PlayerMovement.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
     public VariableJoystick variableJoystick;
     public Rigidbody rb;
 
+    [SerializeField] private float speed;
+    [SerializeField] private float angle;
+    
+    [SerializeField] private Vector3 offset;
+    
     private void Start()
     {
         InitScript();
@@ -14,9 +18,13 @@ public class PlayerMovement : MonoBehaviour
     public void FixedUpdate()
     {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg - 90;
+        
+        transform.eulerAngles = new Vector3(0, -angle, 0);
+        
         rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
-        Camera.main.transform.position = transform.position + new Vector3(0, 12.5f, -20f);
+        Camera.main.transform.position = transform.position + offset;
     }
 
     private void InitScript()
@@ -32,5 +40,10 @@ public class PlayerMovement : MonoBehaviour
 
         variableJoystick.SetMode(JoystickType.Dynamic);
         variableJoystick.AxisOptions = AxisOptions.Both;
+    }
+
+    public void Stop()
+    {
+        rb.velocity = Vector3.zero;
     }
 }

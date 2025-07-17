262f436871777bd015477d4007e1913c1be6eba5
thanhdhyk117 <nguyentrungthanh12h@gmail.com>
2025-07-17 17:05:47 +0700

    + Add new script

---
+++ Assets/__Game/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    private Level level;
+    
+    private Level currentLevel => level;
+    
+    public Vector3 FinishPoint => currentLevel.finishPoint.position;
+}
---
+++ Assets/__Game/Scripts/StateMachine/PatrolState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState : IState<Bot>
+{
+    int targetBrick;
+    public void OnEnter(Bot t)
+    {
+        t.ChangeAnimation(Consts.ANIM_RUN);
+        targetBrick = Random.Range(2, 7);
+        SeekTarget(t);
+    }
+
+    public void OnExcute(Bot t)
+    {
+        if (t.IsDestination)
+        {
+            if (t.BrickCount >= targetBrick)
+            {
+                t.ChangeState(new AttackState());
+            }
+            else
+            {
+                SeekTarget(t);
+            }
+        }
+    }
+
+    public void OnExit(Bot t)
+    {
+    }
+
+    private void SeekTarget(Bot t)
+    {
+        if (t.stage != null)
+        {
+            Brick brick = t.stage.SeekBrickPoint(t.colorType);
+
+            if (brick == null)
+            {
+                t.ChangeState(new AttackState());
+            }
+            else
+            {
+                t.SetDestination(brick.TF.position);
+            }
+        }
+        else
+        {
+            t.SetDestination(t.TF.position);
+        }
+    }
+
+}
